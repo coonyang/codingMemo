fd537fdc994e78e0b25b0b8a5d8bd76d25d6ede8
Hajun Jeon <gkwns1231@naver.com>
2026-06-13 03:45:26 +0900

    두 수의 합 추가 방법

--- 코딩테스트연습/두 수의 합.js
+++ 코딩테스트연습/두 수의 합.js
@@ -8,11 +8,24 @@ function twoSum(nums, target) {
     const diff = target - nums[i];
 
     if (map[diff] !== undefined) {
+      // target - nums[i] 값을 map 오브젝트에서 찾음
       return [map[diff], i];
     }
 
-    map[nums[i]] = i;
+    map[nums[i]] = i; // 못찾았으면 map에 해당 값 추가 // 맨 처음에 2가 들어오면 map에 { 2 : 0 } 으로 저장됨
   }
 }
 
 console.log(twoSum([2, 7, 11, 15], 9));
+
+// 각각 비교하는 다른 방법
+
+function towSum2(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [nums[i], nums[j]];
+      }
+    }
+  }
+}
